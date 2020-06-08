3e1530ea0f21e223a0d4d9f15e023f1065da8630
takuya hakiri <tyapi4@yahoo.co.jp>
2020-06-08 15:51:52 +0900

    fix: jsを修正

--- app/javascript/packs/toppage.js
+++ app/javascript/packs/toppage.js
@@ -1,12 +1,4 @@
 $(function () {
-  $("#nav").load("header.html");
-  $("#section1").load("top.html");
-  $("#section2").load("vision.html");
-  $("#section3").load("greeting.html");
-  $("#section4").load("support_for_venture_companies.html");
-  $("#section5").load("business.html");
-  $("#section6").load("company_profile.html");
-  $("#footer").load("footer.html");
 
   var current;
   $.scrollify({
