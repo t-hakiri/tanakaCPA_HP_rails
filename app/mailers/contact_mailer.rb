f22988facb81d3ef3d9e9251757b8b90dc4a5f9e
takuya hakiri <tyapi4@yahoo.co.jp>
2020-06-09 14:35:16 +0900

    add: お問い合わせのmailer機能実装

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,17 @@
 class ContactMailer < ApplicationMailer
+  def send_mail(name, email, subject, body)
+    @name = name
+    @email = email
+    @subject = subject
+    @body = body
+
+    mail(
+      from: @email,
+      to:   'test@aa.jp',
+      subject: @subject
+    )
+  end
 end
+
+
+# to:   'info@tanaka-accounting.jp',
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tanaka#toppage'
-  get  "contact"  => "tanaka#contact"
+  get 'contact', to: 'tanaka#contact'
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
